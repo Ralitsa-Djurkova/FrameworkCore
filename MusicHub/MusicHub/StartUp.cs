47b4cb3a6845c9a0a3547de5bdd11a486450dd8d
Ralitsa-Djurkova <74554806+Ralitsa-Djurkova@users.noreply.github.com>
2023-08-20 13:38:57 +0300

    .

--- MusicHub/MusicHub/StartUp.cs
+++ MusicHub/MusicHub/StartUp.cs
@@ -1,11 +1,9 @@
 ﻿namespace MusicHub
 {
     using System;
-    using System.Linq;
-    using System.Text;
+
     using Data;
     using Initializer;
-    using MusicHub.Data.Models;
 
     public class StartUp
     {
@@ -22,42 +20,7 @@
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            StringBuilder sb = new StringBuilder(); 
-            var albumsInfo = context.Albums
-                 .Where(a => a.ProducerId == producerId)
-                 .Select(a => new
-                 {
-                     AlbumsName = a.Name,
-                     ReleasData = a.ReleaseDate.ToString("MM/dd/yyyy"),
-                     ProduserName = a.Name,
-                     Song = a.Songs
-                     .Select(a => new
-                     {
-                         SongName = a.Name,
-                         Price = a.Price.ToString("f2"),
-                         Writer = a.Writer.Name
-                     })
-                     .OrderByDescending(a => a.SongName)
-                     .ThenBy(a => a.Writer)
-                     .ToArray(),
-                     TotalAlbumPrice = a.Price.ToString("f2")
-
-                 })
-                 .ToArray();
-
-            foreach (var album in albumsInfo)
-            {
-                sb.
-                    AppendLine($"-AlbumName: {album.AlbumsName}")
-                    .AppendLine($"-ReleaseDate: {album.ReleasData}")
-                    .AppendLine($"-ProducerName: {album.ProduserName}")
-                    .AppendLine($"-Songs: ");
-
-                foreach (var song in album.Song)
-                {
-
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
